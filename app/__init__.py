071043e6a8bce2024ffd870a09cca0e82b585343
Brian Cornally <brian.cornally>
2021-09-12 11:27:33 +0800

    tidy

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,13 @@
-from flask import Flask, url_for
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
+from flask import Flask, url_for
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
 login=LoginManager(app)
 login.login_view='patient_login' # specify the login page to redirect when require login
 
 from app import routes,models
+
