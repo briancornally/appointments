8b264ae7e97029548bc0a2c425fa7730c4d67e55
Brian Cornally <brian.cornally>
2021-09-12 11:27:13 +0800

    add priority

--- populate.py
+++ populate.py
@@ -3,6 +3,7 @@ from app import db
 from app.models import *
 from datetime import *
 from pprint import pprint as pp
+from datetime import datetime
 
 print("* recreating database ...")
 db.drop_all()
@@ -29,23 +30,24 @@ db.session.commit()
 print(doctor.query.all())
 
 appointment_data=[
-    ("D1","P1","08 03 2018 09:00:00"),
-    ("D1","P1","08 04 2018 10:00:00"),
-    ("D1","P2","08 03 2018 10:00:00"),
-    ("D1","P1","08 04 2018 11:00:00"),
-    ("D2","P1","18 03 2018 08:00:00"),
-    ("D2","P1","18 04 2018 09:00:00"),
-    ("D2","P3","18 03 2018 09:00:00"),
-    ("D2","P3","18 04 2018 10:00:00")
+    ("D1","P1","08 03 2018 09:00:00","H"),
+    ("D1","P1","08 04 2018 10:00:00","H"),
+    ("D1","P2","08 03 2018 10:00:00","H"),
+    ("D1","P1","08 04 2018 11:00:00","H"),
+    ("D2","P1","18 03 2018 08:00:00","H"),
+    ("D2","P1","18 04 2018 09:00:00","H"),
+    ("D2","P3","18 03 2018 09:00:00","H"),
+    ("D2","P3","18 04 2018 10:00:00","H")
 ]
-db.session.rollback()
-(doctor_name,patient_name,date_string) = appointment_data[0]
-for (doctor_name,patient_name,date_string) in appointment_data:
+
+(doctor_name,patient_name,date_string,priority) = appointment_data[0]
+for (doctor_name,patient_name,date_string,priority) in appointment_data:
     date_time=datetime.strptime(date_string, "%d %m %Y %H:%M:%S")
     patient=Patient.query.filter_by(patient_name=patient_name).first()
     doctor=Doctor.query.filter_by(doctor_name=doctor_name).first()
-    appointment=Appointment(patient_id=patient.id,doctor_id=doctor.id,date_time=date_time)
+    appointment=Appointment(patient_id=patient.id,doctor_id=doctor.id,date_time=date_time,priority=priority)
     db.session.add(appointment)
     db.session.commit()
 pp(appointment.query.all())
 print("* database populated")
+
